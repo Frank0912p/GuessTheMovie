fe1898111855aebbec368040b4cebadc55e767b8
Francis <mai.fran0912@gmail.com>
2025-01-03 22:45:25 +0100

    Initial commit

---
+++ src/Juego.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.File;
+
+public class Juego {
+    private int intentosRestantes;
+    private final String peliculas;
+    private String tituloEscogido;
+    private String tituloEscogidoOculto;
+    private final char[] letrasAcertadas;
+    private int contadorAciertos;
+    private final Scanner scanner;
+}
